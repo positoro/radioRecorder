8d685c364f69d52b3f673cd67920ee86511f2531
tomo <positoro@gmail.com>
2020-12-11 14:02:46 +0900

    simpling tabler

--- src/tabler.py
+++ src/tabler.py
@@ -9,34 +9,30 @@ date = datetime.date.today()
 
 all_results = pd.DataFrame()
 
+url = localModule.URL_OF_API + '/{0}/radio/{1}.json?key={2}'.format(localModule.AREA, date, localModule.KEY_OF_API)
+request_get = requests.get(url)
 
-import requests
-res = requests.head('https://www.kantei.go.jp')
+if request_get.status_code != 200:
+  print('can not get data')
 
-for service in localModule.SERVICES:
-  url = localModule.URL_OF_API + '/{0}/{1}/{2}.json?key={3}'.format(localModule.AREA, service, date, localModule.KEY_OF_API)
-  request_get = requests.get(url)
+getted_json_data = request_get.json()
 
-  #print(html_timestamp)
-  #print(res.headers['Last-Modified'])
-  #html_timestamp = datetime.datetime.strptime(res.headers['Last-Modified'], "%a, %d %b %Y %H:%M:%S GMT")
+r1_result = pd.json_normalize(getted_json_data['list']['r1'])
+r2_result = pd.json_normalize(getted_json_data['list']['r2'])
+r3_result = pd.json_normalize(getted_json_data['list']['r3'])
+
+all_results = pd.concat([r1_result, r2_result, r3_result])
+all_results.drop_duplicates(subset='title', inplace=True)
 
 
-  if request_get.status_code != 200:
-    print('can not get data')
-    break
- 
-  getted_json_data = request_get.json()
-  result = pd.json_normalize(getted_json_data['list'][service])
-
-  all_results = pd.concat([all_results, result])
+#----
 
 all_results = all_results[~all_results['title'].str.contains('放送休止')]
-all_results = all_results.reset_index(drop=True)
-#all_results.drop_duplicates(subset='title', inplace=True)
 
 all_results['title'] = all_results['title'].apply(lambda x: '_'.join(x.split()))
 all_results['title'] = all_results['title'].apply(lambda x: x.replace("\u25BD", '_'))
+
+all_results = all_results.reset_index(drop=True)
 
 #----
 
