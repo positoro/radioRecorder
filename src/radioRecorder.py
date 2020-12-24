b15c55b22f43b616f81f0d33c19f4947408a1af5
tomo <positoro@gmail.com>
2020-12-24 14:14:07 +0900

    delete no use file

--- src/radioRecorder.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-all_results.to_csv('./results.csv', index=None)
-
-#ffmpeg -i https://nhkradiobkr1-i.akamaihd.net/hls/live/512291/1-r1/1-r1-01.m3u8 -t 900 -movflags faststart -c copy -bsf:a aac_adtstoasc r.m4a
