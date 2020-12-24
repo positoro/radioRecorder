7958f53be3c82d84512a5d25ec7164eb8b6dfcad
root <root@localhost>
2020-12-24 20:39:01 +0900

    coding get token

--- src/localModuleForMinpou.py
+++ src/localModuleForMinpou.py
@@ -16,6 +16,42 @@ RADIKO_AUTHKEY_VALUE='bcd151073c03b352e1ef2fd66c32209da9ca0afa'
 RADIKO_AUTH1_FMS_URL='https://radiko.jp/v2/api/auth1'
 RADIKO_AUTH2_FMS_URL='https://radiko.jp/v2/api/auth2'
 
+
 #----
 
 TABLE_DAYS = 2
+
+#----
+
+def get_auth_token():
+
+  headers_for_get_auth1_fms = {
+     'pragma' : 'no-cache',
+     'X-Radiko-App' : 'pc_html5',
+     'X-Radiko-App-Version' : '0.0.1',
+     'X-Radiko-User' : 'test-stream',
+     'X-Radiko-Device' : 'pc'
+  }
+
+  response = requests.get(localModuleForMinpou.RADIKO_AUTH1_FMS_URL, headers=headers_for_get_auth1_fms)
+
+  auth_token = response.headers['X-Radiko-AuthToken']
+  key_length = int(response.headers['X-Radiko-KeyLength'])
+  key_offset = int(response.headers['X-Radiko-KeyOffset'])
+
+  auth_token_slice = localModuleForMinpou.RADIKO_AUTHKEY_VALUE[key_offset:key_offset + key_length]
+  partial_key = base64.b64encode(auth_token_slice.encode())
+
+#----
+  
+  headers_for_get_auth2_fms = {
+     'pragma' : 'no-cache',
+     'X-Radiko-AuthToken' : auth_token,
+     'X-Radiko-PartialKey' : partial_key,
+     'X-Radiko-User' : 'test-stream',
+     'X-Radiko-Device' : 'pc'
+  }
+
+  response = requests.get(localModuleForMinpou.RADIKO_AUTH2_FMS_URL, headers=headers_for_get_auth2_fms)
+
+  return auth_token
