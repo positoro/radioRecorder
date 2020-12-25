64b269d5f375f2618b09a9a0f0f35d822743cf37
tomo <positoro@gmail.com>
2020-12-26 01:30:40 +0900

    merge

--- src/recorder_for_minpou.py
+++ src/recorder_for_minpou.py
@@ -87,4 +87,4 @@ ffmpeg_command_line = 'ffmpeg \
 command_line = "{0}".format(ffmpeg_command_line)
 res = subprocess.check_output(command_line, shell=True)
 
-tagging_cover_art(image_url, data_file_name)
+#tagging_cover_art(image_url, data_file_name)
