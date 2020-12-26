b2577b6a86db100440fa013dceb4ea8d1a8b01c0
tomo <positoro@gmail.com>
2020-12-26 19:01:14 +0900

    misc

--- src/program_selector_for_minpou.py
+++ src/program_selector_for_minpou.py
@@ -6,19 +6,29 @@ table = pd.read_csv(localModuleForMinpou.TABLE_FILE)
 table_NHK_R1 = table[table['station_id'] == 'JOAK']
 table_NHK_FM = table[table['station_id'] == 'JOAK-FM']
 
+table_MINPOU = table[
+  (table['station_id'] != 'JOAK') &
+  (table['station_id'] != 'JOAK-FM')
+]
+
+#-------------
+
+
 table_title_keywords = [
   'ニュース',
 ]
 
-table_MINPOU = pd.DataFrame()
 
+#-------------
+
+table_MINPOU_SELECTED = pd.DataFrame()
 for keyword in table_title_keywords:
-  table_MINPOU = pd.concat([table_MINPOU, table[table['title'].str.contains(keyword)]])
+  table_MINPOU_SELECTED = pd.concat([table_MINPOU_SELECTED, table_MINPOU[table_MINPOU['title'].str.contains(keyword)]])
 
 selected_results = pd.concat([
   table_NHK_R1,
   table_NHK_FM,
-  table_MINPOU,
+  table_MINPOU_SELECTED,
 ])
 
 selected_results.to_csv(localModuleForMinpou.TABLE_FILE, index=None)
